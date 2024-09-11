7d47a624444778fb55bfeb24416a7b43381dd535
dhrithi m gowda <2023ci_dhrithimgowda_a@nie.ac.in>
2024-09-11 15:55:12 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+def add_numbers(request):
+    result = None
+    error = None
+    if request.method == "POST":
+        num1 = request.POST.get('num1')
+        num2 = request.POST.get('num2')
+
+        try:
+            sum = float(num1) + float(num2)
+            result = f"the sum is: { sum }"
+          
+        except:
+            error = "something went wrong"
+         
+
+    return render(request, 'add.html', {'result':result,'error':error})
